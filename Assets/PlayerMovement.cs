b0ec66be61581fb4d09ec0ae86d4fac65eb27bb6
JayBlonski <jjayblonski@gmail.com>
2023-03-17 13:56:33 +0100

    Added keyboard movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,12 +6,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float movement_speed, rotation_speed;
     private PlayerInput input;
     private InputAction followAction;
     private InputAction walkAction;
     private InputAction runAction;
     private InputAction keyAction;
+    private Ray ray;
+    private RaycastHit hit;
+
 
     NavMeshAgent navPlayer;
     Vector3 transPlayer;
@@ -28,49 +31,36 @@ public class PlayerMovement : MonoBehaviour
         keyAction = input.actions["Keys"];
         
     }
-    /*private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            mouse++;
-            Debug.Log(mouse);
-            Move();
-        }
-        if(Input.GetMouseButtonUp(0))
-        {
-
-        }
-    }*/
 
     private void Update()
     {
-            Vector3 key = keyAction.ReadValue<Vector3>();
             walkAction.performed += _ => Move(3.5f);
             runAction.performed += _ => Move(10f);
             
             if(followAction.phase == InputActionPhase.Performed) { Move(5f);  }
             if(followAction.phase == InputActionPhase.Canceled) { Stop();  }
 
-            if(keyAction.phase == InputActionPhase.Performed)
-            {
-            Debug.Log("Hemlo");
-            Vector3 test = new Vector3(0, 25, 0);
-            if (key.x != 0)
-                transform.Rotate(test);
-                if (key.z != 0)
-                {
-                    Debug.Log("¯yjê");
-                    //transform.position += transform.forward * key.z * Time.deltaTime * navPlayer.speed;
+            var i = 0;
+            while(i < 10){
+                if(Input.GetKey("up") || Input.GetKey("w")){
+                    transform.position += transform.forward * Time.deltaTime * movement_speed;
                 }
+                if(Input.GetKey("down") || Input.GetKey("s")){
+                    transform.position += -transform.forward * Time.deltaTime * movement_speed;
+                }
+                if(Input.GetKey("left") || Input.GetKey("a")){
+                    transform.Rotate(0f, -rotation_speed * Time.deltaTime, 0f);
+                }
+                if(Input.GetKey("right") || Input.GetKey("d")){
+                    transform.Rotate(0f, rotation_speed * Time.deltaTime, 0f);
+                }
+            i++;
             }
     }
-
     private void Move(float speed)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
         {
             navPlayer.speed = speed;
             navPlayer.destination = hit.point;
