7ef49017e2b86af8180a047837ddb6a74458a568
JayBlonski <jjayblonski@gmail.com>
2023-11-10 13:08:12 +0100

    Added NPC Static animation menager

---
+++ Assets/Scripts/Entities/NPCAnimationCenter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimationCenter : MonoBehaviour
+{
+     [SerializeField] Animator animator;
+     [SerializeField] int isBartering;
+     [SerializeField] int isSitting;
+    // Start is called before the first frame update
+    void Start()
+    {
+         animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBartering == 1)
+        {
+            animator.SetBool("isBartering",true);
+        }
+        if(isSitting == 1)
+        {
+            animator.SetBool("isSittingHands",true);
+        }
+    }
+}
